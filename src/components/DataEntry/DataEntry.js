b404efd4244cb3002722a030c12eedc417a670e2
Audry Wolters <audrywolters@gmail.com>
2022-03-02 19:00:09 -0600

    working w/o archaic form stuff

--- src/components/DataEntry/DataEntry.js
+++ src/components/DataEntry/DataEntry.js
@@ -3,17 +3,40 @@ import '../DataEntry/DataEntry.css';
 
 class DataEntry extends Component {
 
+    // AUDRY - this is an archaic way of form
+    // there is a way to ditch the unseccary = (evt) =
+
+    state = {
+        animal: '',
+        color: ''
+    }
+
+    handleInputChange = ( event, propertyName ) => {
+
+        this.setState({
+            [propertyName]: event.target.value
+        })
+    }
+
+    printFavoriteThings = ( event ) => {
+
+        console.log( `Your favorite thing is a ${ this.state.color } ${ this.state.animal }` );
+
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="dataEntry">
                 <div>Enter your favorite:</div>
-                <label>Animal</label>
-                <input type='text' />
-                
-                <label className="inputColor">Color</label>                
-                <input type='text' />
 
-                <button>Save</button>
+                <label>Animal</label>
+                <input type='text' onChange={ (event) => this.handleInputChange( event, 'animal' )} />
+                
+                <label className='inputColor'>Color</label>                
+                <input type='text' onChange={ (event) => this.handleInputChange( event, 'color') } />
+
+                <button onClick={ this.printFavoriteThings }>Save</button>
             </div>
         )
     }
